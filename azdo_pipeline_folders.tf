ac8524399e6739350e28e492814d7e0eaa11231b
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2023-05-08 22:47:12 +0200

    added code for pipeline folders

---
+++ azdo_pipeline_folders.tf
@@ -0,0 +1,9 @@
+resource "azuredevops_build_folder" "az_firewall" {
+  project_id  = azuredevops_project.azure_iac_bicep.id
+  path        = "\\Az Firewall"
+}
+
+resource "azuredevops_build_folder" "alz_accelerator" {
+  project_id  = azuredevops_project.azure_iac_bicep.id
+  path        = "\\ALZ - Accelerator"
+}
